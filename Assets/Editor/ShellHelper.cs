c9b71bb36e6624eabfbef8d2fbdb71f6f068d9a4
Akshay Khandizod <akshaykhandizod97@gmail.com>
2020-02-27 23:17:44 +0530

    Basic function added

---
+++ Assets/Editor/ShellHelper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+
+public class ShellHelper
+{
+    private static ShellHelper _instance;
+
+    public static ShellHelper Instance
+    {
+        get { return _instance ?? (_instance = new ShellHelper()); }
+        set { _instance = value; }
+    }
+
+    private Process _process;
+    private ShellHelper()
+    {
+       _process = new Process()
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = "/bin/bash",
+                Arguments = $"-c \"\"",
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = false,
+                RedirectStandardInput = true
+            }
+        };
+
+        _process.Start();
+    }
+
+    public string Bash(string cmd)
+    {
+        var escapedArgs = cmd.Replace("\"", "\\\"");
+     
+        _process.StartInfo = new ProcessStartInfo
+        {
+            FileName = "/bin/bash",
+            Arguments = $"-c \"{escapedArgs}\"",
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true,
+            RedirectStandardInput = true
+        };
+        _process.Start();
+        
+        string result = _process.StandardOutput.ReadToEnd();
+        _process.WaitForExit();
+
+        return result.Trim();
+    }
+}
---
+++ Assets/Editor/TerminalWindow.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEditor;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class TerminalWindow : EditorWindow
+{
+    private string command = "Hello World";
+    private string output = "";
+
+    private string userName="";
+    private string pwdName;
+    private string prefix;
+    [MenuItem ("Window/Terminal Window")]
+    public static void  ShowWindow () {
+        var window = GetWindow(typeof(TerminalWindow)) as TerminalWindow;
+        window.userName = ShellHelper.Instance.Bash( "whoami");
+        window.pwdName = ShellHelper.Instance.Bash("pwd");
+        window.prefix =  window.userName+ " :$"+window.pwdName;
+    }
+    private void OnGUI()
+    {
+        GUILayout.Label (userName, EditorStyles.boldLabel);
+        command = EditorGUILayout.TextField (command);
+        
+        EditorGUILayout.BeginHorizontal();
+        if (GUILayout.Button("Run"))
+        {
+            
+            output = ShellHelper.Instance.Bash(command);
+            prefix =  userName+ " :$"+pwdName;
+            Debug.Log(output);
+        }
+        EditorGUILayout.EndHorizontal();
+    }
+}
